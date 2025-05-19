bafb2593f5caf3f3bcc259eed5044d18142fb9d2
Dani Vainstein <solmarkn@gmail.com>
2025-05-19 18:07:45 -0500

    all cleo classes
    cleo classes and files , not fully implemented

---
+++ src/core/cleo/io/inputs/string_input.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from core.cleo.helpers import tokenize
+from core.cleo.io.inputs.argv_input import ArgvInput
+
+
+class StringInput(ArgvInput):
+    """
+    Represents an input provided as a string
+    """
+
+    def __init__(self, input: str) -> None:
+        super().__init__([])
+
+    def _tokenize(self, input: str) -> list[str]:
+        ...
